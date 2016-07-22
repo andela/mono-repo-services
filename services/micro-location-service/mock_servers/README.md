9141414dbd8048aedeebe3aeaabb5981514de8e0
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-07-22 18:53:38 +0100

    refactor(mocks): remove mock_servers

--- services/micro-location-service/mock_servers/README.md
+++
@@ -1,2 +0,0 @@
-# Andela systems mock microservices
-This repo contains a mock of all andela microservices for testing purposes. A microservice that depends on another can jsut use it's mock during unit/integration test.
--- services/micro-location-service/mock_servers/authentication/index.js
+++
@@ -1,6 +0,0 @@
-const server = require('./server');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
-
-server.start();
-console.log('server running on:', config.authentication);
