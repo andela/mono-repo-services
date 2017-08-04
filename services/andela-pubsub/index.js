0b1f3bfa4379f1a02f9d4b92f042e450922946c3
Ikem Okonkwo <ikem.okonkwo@andela.com>
2017-08-04 09:40:42 +0100

    feat: log proper json error message

--- services/andela-pubsub/index.js
+++ services/andela-pubsub/index.js
@@ -2,6 +2,30 @@ const producer = require('./producer');
 const logger = require('winston');
 const background = require('./background');
 
+const podName = process.env.POD_NAME || 'no-pod-name'
+
+function logErr(attributes, handlerName, err) {
+  console.log(
+    JSON.stringify({
+      eventTime: (new Date()).toISOString(),
+      message: err.stack,
+      severity: 'ERROR',
+      serviceContext: {
+        service: podName.split('-')[0],
+        version: podName.split('-')[1],
+      },
+      context: {
+        httpRequest: {
+          method: 'POST',
+          url: handlerName,
+          referrer: attributes.eventType,
+        },
+        user: attributes.authorEmail,
+      }
+    })
+  )
+}
+
 function subscribe(options, handlers) {
   // Subscribe to Cloud Pub/Sub and receive messages to process.
   // The subscription will continue to listen for messages until the process
@@ -20,15 +44,15 @@ function subscribe(options, handlers) {
     if (handler && process.env.NODE_ENV !== 'test') {
       handler(payload, (_err) => {
         if (_err) {
-          logger.error(_err.message);
+          logErr(message.attributes, handler.name, _err);
         } else {
-          logger.info('finished processing ', 
-            message.attributes.eventType, 
+          logger.info('finished processing ',
+            message.attributes.eventType,
             ', key ', message.data.id
           );
           message.ack();
         }
-      });
+      }, message.attributes);
     } else {
       message.ack();
     }
