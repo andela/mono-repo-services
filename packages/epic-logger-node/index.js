12e394d2d2bd4e564177027be44880296643038f
Collins Wekesa <collins.wekesa@andela.com>
2017-08-17 19:18:08 +0300

    style code block

--- packages/epic-logger-node/index.js
+++ packages/epic-logger-node/index.js
@@ -4,26 +4,28 @@ const winstonBugsnag = require('winston-bugsnag');
 const env = process.env.NODE_ENV || 'development';
 const VError = require('verror').VError;
 
-const podName = process.env.POD_NAME || 'myService-1'
-const values = podName.split('-')
-const serviceContext = { service: values[0], version: values[1] };
+const podName = process.env.POD_NAME || 'myService-1';
+const values = podName.split('-');
+const serviceContext = {
+  service: values[0],
+  version: values[1]
+};
 
 let format = 'text';
 let level = 'debug';
-if (env === 'staging' || env === 'production' ) {
+if (env === 'staging' || env === 'production') {
   format = 'json';
   level = 'info';
-};
+}
 
 winston.configure({
-    transports: [
-    new EpicTransport(
-      { format,
-        level,
-        serviceContext,
-        levels: winston.config.syslog.levels,
-      }
-    ),
+  transports: [
+    new EpicTransport({
+      format,
+      level,
+      serviceContext,
+      levels: winston.config.syslog.levels
+    }),
   ],
 });
 
@@ -37,13 +39,13 @@ winston.setLevels(winston.config.syslog.levels);
 // monkey patch error and crit winston methods
 ['error', 'crit'].forEach((target) => {
   var method = winston[target];
-  winston[target] = function() {
+  winston[target] = function () {
     let args = Array.prototype.slice.call(arguments);
     if (args && args[0] && args[0] instanceof VError) {
       args = [VError.fullStack(args[0])].concat(args.slice(1));
     }
     method.apply(this, args);
-  }
+  };
 });
 
 module.exports = winston;
